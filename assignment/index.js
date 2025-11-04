92136f31798aa6e25d72e41b309061a25b50980c
Rahul Jadhav <rahul.jadhav@thinkitive.com>
2025-11-04 14:18:59 +0530

    Added Assignment files

---
+++ assignment/index.js
@@ -0,0 +1,48 @@
+document.getElementById("customer").addEventListener("click", loadCustomer);
+console.log("customer button clicked");
+document.getElementById("images").addEventListener("click", loadImages);
+console.log("images button clicked");
+
+async function loadCustomer() {
+  await fetch("https://playground.mockoon.com/customers")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.name);
+      const tableBody = document.getElementById("customer-table-body");
+      tableBody.innerHTML = "";
+      data.forEach((customer) => {
+        const row = `
+
+                <tr>
+                    <td>${customer.name}</td>
+                    <td>${customer.email}</td>
+                    <td>${customer.phone}</td>
+            `;
+        tableBody.innerHTML = tableBody.innerHTML + row;
+      });
+    })
+    .catch((error) => console.error("Error loading customer data:", error));
+}
+
+async function loadImages() {
+  await fetch("https://playground.mockoon.com/photos")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const tableBody = document.getElementById("image-table-body");
+      tableBody.innerHTML = "";
+      data.forEach((image) => {
+        const row = `
+            
+                        <tr>
+                            <td>${image.caption}</td>
+                            <td>${image.likes}</td>
+                            <td>${image.url}</td>
+                        </tr>
+                    `;
+        tableBody.innerHTML = tableBody.innerHTML + row;
+      });
+    })
+    .catch((error) => console.error("Error loading images:", error));
+}
